5cfcf20416149864830709d33b07c5318363b1e4
mishitokun <toshimishito000@gmail.com>
2019-05-21 01:44:16 +0900

    create post controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    # if params[:tag]
+    #   @posts = Post.tagged_with(params[:tag])
+    # # ransack用
+    # elsif params[:q]
+    # #  @q = Post.ransack(params[:q])
+    # #  @posts = @q.result.page(params[:page])
+    # else
+    #   @posts = Post.all
+    # end
+    #   @posts = @posts.order('created_at DESC').page(params[:page])
+    @posts = Post.all
+    @posts = @posts.order('created_at DESC').page(params[:page])
   end
 end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
