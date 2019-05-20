004e2652e030c5038c1df488800bfe11c38da2fd
mishitokun <toshimishito000@gmail.com>
2019-05-21 03:09:03 +0900

    create Comment controller #3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :content, presence:true, length: { maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable, :omniauthable, omniauth_providers: [:twitter]
 
   has_many :posts
+  has_many :comments
 
   # relationships
   has_many :relationships
