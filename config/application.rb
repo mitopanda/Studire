0012ed065d6866d6d6408cdfca0cedad03d622a6
mishitokun <toshimishito000@gmail.com>
2019-05-23 20:25:26 +0900

    Rspec Preparation

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,14 @@ module Studire
     config.i18n.default_locale = :ja
 
     config.time_zone = 'Tokyo'
+
+    #rspec
+    config.generators do |g|
+      g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
