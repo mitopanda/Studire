e643dbf2fd40ea40b341eb6e90d4fda73acbae2f
mishitokun <toshimishito000@gmail.com>
2019-05-29 19:24:23 +0900

    change rspec label

--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -6,11 +6,11 @@ describe 'サインアップ機能', type: :system, js: true do
   context '有効な情報を送信したとき' do
     it 'トップページにリダイレクトされる' do
       visit new_user_registration_path
-      fill_in 'name', with: 'Test'
-      fill_in 'email', with: 'test@user.com'
-      fill_in 'password', with: 'password'
-      fill_in ('password-confirmation'), with: 'password'
-      click_button 'Sign up'
+      fill_in '名前', with: 'Test'
+      fill_in 'メールアドレス', with: 'test@user.com'
+      fill_in 'パスワード', with: 'password'
+      fill_in ('パスワードの確認'), with: 'password'
+      click_button '会員登録'
       expect(page).to have_css('div.alert.alert-info')
     end
   end
