fb048490faa3c95f725d9d9ff9549bdbfb3e6e2d
mishitokun <toshimishito000@gmail.com>
2019-05-31 01:25:41 +0900

    modify sign_up_spec

--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -6,10 +6,10 @@ describe 'サインアップ機能', type: :system, js: true do
   context '有効な情報を送信したとき' do
     it 'トップページにリダイレクトされる' do
       visit new_user_registration_path
-      fill_in 'Name', with: 'Test'
-      fill_in 'Email', with: 'test@user.com'
-      fill_in 'Password', with: 'password'
-      fill_in ('Password_confirmation'), with: 'password'
+      fill_in 'name', with: 'Test'
+      fill_in 'email', with: 'test@user.com'
+      fill_in 'password', with: 'password'
+      fill_in ('password_confirmation'), with: 'password'
       click_button '会員登録'
       expect(page).to have_css('div.alert.alert-info')
     end
@@ -18,10 +18,10 @@ describe 'サインアップ機能', type: :system, js: true do
   context '無効な情報を送信したとき' do
     it 'エラーが起こる' do
       visit new_user_registration_path
-      fill_in '名前', with: ''
-      fill_in 'メールアドレス', with: ''
-      fill_in 'パスワード', with: ''
-      fill_in ('パスワードの確認'), with: ''
+      fill_in 'name', with: ''
+      fill_in 'email', with: ''
+      fill_in 'password', with: ''
+      fill_in ('password_confirmation'), with: ''
       click_button '会員登録'
       expect(page).to have_css('div#error_explanation')
       expect(page).to have_content 'メールアドレスを入力してください'
