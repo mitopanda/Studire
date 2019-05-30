3c427417d3864c76b75b319a8fe25bf6c682fcfe
mishitokun <toshimishito000@gmail.com>
2019-05-31 01:11:37 +0900

    Complete user new layout

--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -6,10 +6,10 @@ describe 'サインアップ機能', type: :system, js: true do
   context '有効な情報を送信したとき' do
     it 'トップページにリダイレクトされる' do
       visit new_user_registration_path
-      fill_in '名前', with: 'Test'
-      fill_in 'メールアドレス', with: 'test@user.com'
-      fill_in 'パスワード', with: 'password'
-      fill_in ('パスワードの確認'), with: 'password'
+      fill_in 'Name', with: 'Test'
+      fill_in 'Email', with: 'test@user.com'
+      fill_in 'Password', with: 'password'
+      fill_in ('Password_confirmation'), with: 'password'
       click_button '会員登録'
       expect(page).to have_css('div.alert.alert-info')
     end
