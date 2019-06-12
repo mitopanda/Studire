7f6e99d5523db42baa307691ac4ca20e450f0a77
mishitokun <toshimishito000@gmail.com>
2019-06-13 03:42:29 +0900

    rails_adminを権限ユーザに限定

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -30,5 +30,9 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    if user && user.admin_flg?
+      can :access, :rails_admin
+      can :manage, :all
+    end
   end
 end
