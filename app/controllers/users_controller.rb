41f90c459feb0825d6736404ef35199abeccaee6
mishitokun <toshimishito000@gmail.com>
2019-06-13 20:20:19 +0900

    Add likes page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,8 @@ class UsersController < ApplicationController
     @liking = @user.liked_posts.page(params[:page]).per(10)
     counts(@user)
   end
+
+  def likes
+    @liking = current_user.liked_posts.page(params[:page]).per(10)
+  end
 end
