fa7ec7a516c65fa60ecdd2b41bbe2d050c6e94c1
mishitokun <toshimishito000@gmail.com>
2019-06-13 21:19:43 +0900

    likesページの作成&likes内検索・タグ機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,15 @@ class UsersController < ApplicationController
   end
 
   def likes
-    @liking = current_user.liked_posts.page(params[:page]).per(10)
+    @likes = current_user.liked_posts
+    if params[:q]
+      query = { title_or_content_or_book_or_direction_or_summary_cont: params[:q] }
+      @search = @likes.ransack(query)
+      @liking = @search.result.order('created_at DESC').page(params[:page]).per(10)
+    elsif params[:tag]
+      @liking = @likes.tagged_with(params[:tag]).order('created_at DESC').page(params[:page]).per(15)
+    else
+      @liking = @likes.order('created_at DESC').page(params[:page]).per(10)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,5 @@ Rails.application.routes.draw do
   # お気に入り
   resources :favorites, only: [:create, :destroy]
   get 'tags/:tag', to: 'home#index', as: :tag
+  get 'likes/tags/:tag', to: 'users#likes', as: :likes_tag
 end
